0c77d1b66f546cd3391c891697d023f5f7b34221
yeison0797 <yeison07bermudez@gmail.com>
2022-06-24 16:05:58 -0500

    actual

--- src/componentes/IzqProductos.js
+++ src/componentes/IzqProductos.js
@@ -1,40 +1,34 @@
 import React from "react";
 import '../hojas-de-estilo/IzqProductos.css';
-// import { BsSearch } from "react-icons/bs";
-// import { click } from "@testing-library/user-event/dist/click";
+import { BsSearch } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
+
 
 export function IzqProductos(){
-  const btn = document.querySelector('.btn'),
-    input = document.querySelector('.input');
-
-  btn.addEventListener('click', () => {
-    btn.classList.toggle('close');
-    input.classList.toggle('inclicked');
-  });
   return(
     <div className="izq">
       <div className="busqueda-agregar">
-        <div className="middle">
-          {/* <div className="search-input">
-            <input type='text' icon={<BsSearch />} placeholder="search " className="entrada-busqueda"/>   
-            <i className="iconos"><BsSearch style={{  position:"relative", left:"100%"}} /></i>     
-          </div> */}
-          <form action="" className="search-box">
-            <input type="text" className="input"/>
-            <button className="btn" type="button"></button>
-          </form>
+        
+        <div className="search-input">
+          {/* <i className="iconos"><BsSearch style={{  position:"relative", left:"100%"}} /></i>      */}
+          <input type='text' icon={<BsSearch />} placeholder="search " className="entrada-busqueda"/>   
+          
+        </div> 
+        <div className="espacio2">
         </div>
+        <div className="boton-agregar">
+          <button className="btn-agregar" type="submit">
+            <AiOutlinePlus style={{ fontSize: '2.2vh', color:'white'}}/>
+            <label className="label-agregar">
+              Agregar producto
+            </label>
+          </button>
+        </div>  
       </div>
-      <div className="abajo">
-          adios
+      <div className="contenido-productos">
+        hola
       </div>
+
     </div>
   )
 }
-const btn = document.querySelector('.btn'),
-input = document.querySelector('.input');
-
-btn.addEventListener('click', () => {
-btn.classList.toggle('close');
-input.classList.toggle('inclicked');
-});
