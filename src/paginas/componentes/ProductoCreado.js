1b6db07de4fb7f927baa4ade0c2df5e03a67d8d2
jbcastellanos <bmcastellanos3@gmail.com>
2022-07-01 11:05:30 -0500

    der-update

--- src/paginas/componentes/ProductoCreado.js
+++ src/paginas/componentes/ProductoCreado.js
@@ -2,21 +2,19 @@ import React from "react";
 import './css/ProductoCreado.css';
 
 
-export function ProductoCreado (props){
-  return(
-    <div className="tarjeta">
+export function ProductoCreado(props) {
+  return (
+    <div className="tarjeta px-3">
       {/* checkbox y nombre con id */}
-      <div className="check-nombre">
-        <div class="form-check">
-          <input class="form-check-input bg-dark border-0" type="checkbox" value="" id="flexCheckDefault"/>
-        </div>
-        <div className="nombre-id">
-          <label className="id">ID {console.log(props)}</label>
+      <div className="check-nombre gap-3">
+        <input class=" bg-dark border-0" type="checkbox" value="" id="flexCheckDefault" />
+        <div className="nombre-id d-inline-block">
+          <label className="id d-block">ID SC20222 {console.log(props)}</label>
           <label className="nombre">Nombre del producto</label>
         </div>
       </div>
       {/* cantidad y unidad */}
-      <div className="cantidad-unidad">
+      <div className="cantidad-unidad gap-2">
         <label className="cantidad">200</label>
         <label className="unidad">Unidades</label>
       </div>
